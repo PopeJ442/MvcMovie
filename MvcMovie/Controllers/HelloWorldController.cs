21e9d7c82d44cc036c452fee94ec9baf26ed8de0
popej442 <hpi78generation@gmail.com>
2024-02-07 09:29:19 +0000

    initial Create

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult Welcome(string name,int age = 4)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["age"] = age;
+        return View();
+    }
+}
---
+++ MvcMovie/Controllers/seasonsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class seasonsController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public seasonsController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: seasons
+        public async Task<IActionResult> Index()
+        {
+            return _context.season != null ?
+                        View(await _context.season.ToListAsync()) :
+                        Problem("Entity set 'MvcMovieContext.season'  is null.");
+        }
+
+        // GET: seasons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.season == null)
+            {
+                return NotFound();
+            }
+
+            var season = await _context.season
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (season == null)
+            {
+                return NotFound();
+            }
+
+            return View(season);
+        }
+
+        // GET: seasons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: seasons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description")] season season)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(season);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(season);
+        }
+
+        // GET: seasons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.season == null)
+            {
+                return NotFound();
+            }
+
+            var season = await _context.season.FindAsync(id);
+            if (season == null)
+            {
+                return NotFound();
+            }
+            return View(season);
+        }
+
+        // POST: seasons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description")] season season)
+        {
+            if (id != season.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(season);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!seasonExists(season.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(season);
+        }
+
+        // GET: seasons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.season == null)
+            {
+                return NotFound();
+            }
+
+            var season = await _context.season
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (season == null)
+            {
+                return NotFound();
+            }
+
+            return View(season);
+        }
+
+        // POST: seasons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.season == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.season'  is null.");
+            }
+            var season = await _context.season.FindAsync(id);
+            if (season != null)
+            {
+                _context.season.Remove(season);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool seasonExists(int id)
+        {
+            return (_context.season?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
